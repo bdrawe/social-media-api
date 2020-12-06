a2ee87042c2d5c2295f27528be1c019673adc436
B. Drawe <brycerdrawe.b@gmail.com>
2020-12-06 00:36:48 -0700

    bug fixed and started on user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,2 +1,68 @@
 const { User, Thought } = require('../models');
 
+const { User } = require('../models');
+
+const userController =  {
+    getAllUsers(req, res) {
+        User.find({})
+        .populate({
+            path: "thoughts",
+            select: "thoughtText createdAt reactions reactionCount"
+        })
+        .populate({
+            path: "friends",
+            select: "-__v"
+        })
+        .select( "-__v")
+        .sort({_id: -1 })
+        .then(dbUserData => {
+            res.json(dbUserData)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(400).json(err)
+        });
+    },
+    getOneUserById({params}, res){
+        User.findOne({_id: params.id})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .select("-__v")
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({message: "Cannot find a user with that id"});
+                return
+            }
+            res.json(dbUserData)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(400).json(err)
+        }) 
+    },
+    createUser({body}, res) {
+        User.create(body)
+        .then(dbUserData=>res.json(dbUserData))
+        .catch(err=>res.json(err))
+    },
+    updateUser({params,body}, res){
+        User.findOneAndUpdate({_id: params.id}, body,{new:true, runValidators: true})
+        .then(dbUserData =>{
+            if(!dbUserData){
+                res.status(404).json({message: "Cannot find a user with that id"});
+                return
+            }
+            res.json(dbUserData)
+        })
+        .catch(err =>{
+            console.log(err)
+            res.status(400).json(err)
+        })
+    }
+}
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
-const router = require('express').Router;
+const router = require('express').Router();
 const thoughtRoutes = require('./thought-routes');
-const userRoutes = require('./user-routes');
+// const userRoutes = require('./user-routes');
 
-router.use('/users', userRoutes);
+// router.use('/user', userRoutes);
 router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
